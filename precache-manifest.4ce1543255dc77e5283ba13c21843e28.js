a26dfd8f3f6e0938485572bea4a4720f25b06075
Dmitry Volkov <Dima.volkov1994@gmail.com>
2020-06-23 19:19:33 -0600

    Updates

---
+++ precache-manifest.4ce1543255dc77e5283ba13c21843e28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32f76f9b899cb6385a743309c06a75e",
+    "url": "/Pets-App/index.html"
+  },
+  {
+    "revision": "efb9950c57d9527d4da3",
+    "url": "/Pets-App/static/css/main.31ca773a.chunk.css"
+  },
+  {
+    "revision": "d02b360180f472b91ca6",
+    "url": "/Pets-App/static/js/2.c4c0e8c3.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/Pets-App/static/js/2.c4c0e8c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efb9950c57d9527d4da3",
+    "url": "/Pets-App/static/js/main.5e7bca0d.chunk.js"
+  },
+  {
+    "revision": "8987432121b799750d11",
+    "url": "/Pets-App/static/js/runtime-main.2d47c4a9.js"
+  }
+]);
