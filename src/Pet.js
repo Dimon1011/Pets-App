41d5e96398c1e8f51b55cac75fce74a0ef518c38
Dmitry Volkov <Dima.volkov1994@gmail.com>
2020-06-23 18:34:57 -0600

    added files

--- src/Pet.js
+++ src/Pet.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React from 'react';
+import {Link} from '@reach/router';
 
-const Pet = props => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
-  );
-};
+export default function Pets({name, animal, breed, media, location, id}) {
+    let hero = 'http://placecorgi.com/300/300';
+    if(media.length){
+        hero = media[0].small;
+    }
 
-export default Pet;
+    return (
+        <Link to={`/details/${id}`} className='pet'>
+            <div className='image-container'>
+                <img src={hero} alt={name}/>
+            </div>
+            <div className='info'>
+            <h1>{name}</h1>
+            <h2>{`${animal} - ${breed} - ${location}`}</h2>
+
+            </div>
+        </Link>
+    )
+}
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,36 +1,78 @@
-import React, { useState } from 'react';
-import { ANIMALS } from  '@frontendmasters/pet';
+  
+import React, { useState, useEffect, useContext } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+import Results from "./Results";
+import ThemeContext from "./ThemeContext";
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA");
-  const  [animal, setAnimal] = useState("dog");
+  const [theme, setTheme] = useContext(ThemeContext);
+  const [location, updateLocation] = useState("Seattle, WA");
+  const [breeds, updateBreeds] = useState([]);
+  const [pets, setPets] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
 
-  return  (
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    console.log("animals", animals);
+
+    setPets(animals || []);
+  }
+
+  useEffect(() => {
+    updateBreeds([]);
+    updateBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      updateBreeds(breedStrings);
+    }, console.error);
+    // eslint-disable-next-line
+  }, [animal]); 
+
+  return (
     <div className="search-params">
-    {/* <h1>{location}</h1> */}
-      <form>
-        <lable htmlFor="location">
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
+        <label htmlFor="location">
           Location
-          <input id="location" value={location}
-          placeholder="location" 
-          onChange={e => setLocation(e.target.value)} />
-        </lable>
-        <label htmlFor="animal">
-          animal
-          <select
-            id="animal"
-            value={animal}
-            onChange= {e => setAnimal(e.target.value)}
-            onBlur={e => setAnimal(e.target.value)}>
-            <option>ALL</option>
-            {ANIMALS.map(animal =>(
-              <option value={animal}>{animal}</option>))}
-            </select>
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => updateLocation(e.target.value)}
+          />
         </label>
-        <button>Submit</button>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <label htmlFor="location">
+          Theme
+          <select
+            value={theme}
+            onChange={e => setTheme(e.target.value)}
+            onBlur={e => setTheme(e.target.value)}
+          >
+            <option value="peru">Peru</option>
+            <option value="darkblue">Dark Blue</option>
+            <option value="chartreuse">Chartreuse</option>
+            <option value="mediumorchid">Medium Orchid</option>
+          </select>
+        </label>
+        <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
-  )
-}
+  );
+};
 
 export default SearchParams;
